916e7da38c7f495211b7f848043b4ff895b194c1
ihccenen <ihccenen1@gmail.com>
2025-07-07 17:43:06 -0300

    Add className prop and change label

--- src/app/ui/input.tsx
+++ src/app/ui/input.tsx
@@ -1,5 +1,5 @@
 type InputProps = {
-  label: string,
+  label?: string,
   type?: string,
   id: string,
   name: string,
@@ -7,11 +7,12 @@ type InputProps = {
   defaultValue?: string,
   required: boolean,
   children?: React.ReactNode,
+  className?: string,
 };
 
-export default function Input({ label, type = "text", id, name, pattern, defaultValue = "", required, children = null }: InputProps) {
+export default function Input({ label = "", type = "text", id, name, pattern, defaultValue = "", required, className = "", children = null }: InputProps) {
   return (
-    <div className="relative">
+    <div className={`relative ${className}`}>
       <input
         type={type}
         name={name}
@@ -19,15 +20,15 @@ export default function Input({ label, type = "text", id, name, pattern, default
         pattern={pattern}
         defaultValue={defaultValue}
         required={required}
-        className="block px-2.5 pb-2.5 pt-4 w-full text-sm bg-transparent rounded-lg border-1 appearance-none text-white border-blue-500 invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-500 focus:outline-none focus:ring-0 peer"
+        className="block px-2.5 pb-2.5 pt-4 w-full bg-transparent rounded-lg border-1 appearance-none text-white border-blue-500 invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-500 focus:outline-none focus:ring-0 peer"
         placeholder=" "
       />
-      <label
-        htmlFor={id}
-        className="absolute text-sm text-blue-500 peer-[:invalid&:not(:placeholder-shown)]:text-red-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] peer-focus:bg-gray-900 peer-[&:not(:placeholder-shown)]:bg-gray-900 rounded-md px-2 peer-focus:px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 start-1 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto"
+      { label && <label
+                   htmlFor={id}
+                   className="absolute text-blue-500 peer-[:invalid&:not(:placeholder-shown)]:text-red-500 transform duration-200 -translate-y-[50%] scale-75 top-0 z-10 origin-[0] peer-focus:bg-gray-900 peer-[&:not(:placeholder-shown)]:bg-gray-900 rounded-md px-2 peer-focus:px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-0 peer-focus:scale-75 peer-focus:-translate-y-[50%] start-1 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto"
       >
-        {label}
-      </label>
+                   {label}
+                 </label> }
       {children}
     </div>
   );
