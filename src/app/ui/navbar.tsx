41fdca82106f73f4d2204d95c1c1c166871dc185
ihccenen <ihccenen1@gmail.com>
2025-07-10 01:57:51 -0300

    Remove form action and use onClick, and modify className

--- src/app/ui/navbar.tsx
+++ src/app/ui/navbar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { logout } from "../actions/auth";
 import React from "react";
 
@@ -7,11 +9,15 @@ type NavbarProps = {
 
 export default function Navbar({ username }: NavbarProps) {
   return (
-    <nav className="flex justify-between px-4 py-3 bg-indigo-700">
-      <h1 className="text-3xl font-semibold">{username}</h1>
-      <form action={logout}>
-        <button type="submit" className="text-xl p-1 rounded-md bg-fuchsia-500 hover:bg-fuchsia-600 focus:bg-fuchsia-600 hover:cursor-pointer">Logout</button>
-      </form>
+    <nav className="flex justify-between px-4 py-3 max-sm:px-1 bg-indigo-700">
+      <h1 className="text-3xl max-sm:text-xl font-semibold">{username}</h1>
+      <button
+        type="submit"
+        onClick={() => logout()}
+        className="text-xl max-sm:text-sm p-2 rounded-md bg-fuchsia-500 hover:bg-fuchsia-600 focus:bg-fuchsia-600 hover:cursor-pointer"
+      >
+        Logout
+      </button>
     </nav>
   );
 }
