ba1b2af7832576cc386d473b57cd44c6e0c8160d
ihccenen <ihccenen1@gmail.com>
2025-07-14 19:21:52 -0300

    Refactor to useTransition

--- src/app/ui/navbar.tsx
+++ src/app/ui/navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useTransition } from "react";
 import Button from "./button";
 import { logout } from "../actions/auth";
 
@@ -9,23 +9,18 @@ type NavbarProps = {
 };
 
 export default function Navbar({ username }: NavbarProps) {
-  const [pending, setPending] = useState(false);
+  const [isPending, startTransition] = useTransition();
 
   return (
     <nav className="flex justify-between px-4 py-3 max-sm:px-1 border-b-3 border-neutral-700">
       <h1 className="text-3xl max-sm:text-xl font-semibold">{username}</h1>
       <Button
         type="button"
-        onClick={() => {
-          setPending(true);
-          logout()
-            .then(() => setPending(false))
-            .catch(() => setPending(false));
-        }}
-        className={`text-xl max-sm:text-sm p-2 border-1 border-red-500 ${pending ? "border-red-600 hover:cursor-not-allowed" : "hover:border-red-600 hover:cursor-pointer"}`}
-        isPending={pending}
+        onClick={() => startTransition(async () => logout())}
+        className={`text-xl max-sm:text-sm p-2 border-1 border-red-500 ${isPending ? "border-red-600 hover:cursor-not-allowed" : "hover:border-red-600 hover:cursor-pointer"}`}
+        isPending={isPending}
       >
-        { pending ? "Logging out..." : "Log out" }
+        { isPending ? "Logging out..." : "Log out" }
       </Button>
     </nav>
   );
