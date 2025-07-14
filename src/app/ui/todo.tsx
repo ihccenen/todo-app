8163c9ac32805dc92cca850afceb8fabd2bcacb3
ihccenen <ihccenen1@gmail.com>
2025-07-13 22:16:19 -0300

    Refactor pending state to useTransition

--- src/app/ui/todo.tsx
+++ src/app/ui/todo.tsx
@@ -3,27 +3,22 @@
 import Button from "./button";
 import { deleteTodo, updateTodoStatus } from "../actions/todo";
 import { Todo as TodoType } from "../lib/definitions";
-import { useState } from "react";
+import { useTransition } from "react";
 
 export default function Todo({ todo }: {todo: TodoType }) {
-  const [updatingStatus, setUpdatingStatus] = useState(false);
-  const [deleting, setDeleting] = useState(false);
+  const [updateIsPending, startUpdateTransition] = useTransition();
+  const [deleteIsPending, startDeleteTransition] = useTransition();
 
   return (
     <div className={`text-2xl max-sm:text-xl flex items-center gap-2 p-3 rounded-md ${todo.status === "pending" ? "bg-indigo-900" : "transtion-colors duration-3000 bg-indigo-950"}`}>
       <Button
         type="button"
-        disabled={updatingStatus}
-        onClick={() => {
-          setUpdatingStatus(true);
-          updateTodoStatus(todo)
-            .then(() => setUpdatingStatus(false))
-            .catch(() => setUpdatingStatus(false));
-        }}
+        disabled={updateIsPending}
+        onClick={() => startUpdateTransition(async () => await updateTodoStatus(todo))}
         className={`${todo.status === "completed" ? "text-lime-500 hover:text-lime-600 focus:text-lime-600" : "text-amber-500 hover:text-amber-600 focus:text-amber-600"} w-8 h-8 justify-center shrink-0 rounded-md`}
-        showSpinner={updatingStatus}
+        showSpinner={updateIsPending}
       >
-        { updatingStatus ? null : todo.status === "completed" ? "✔" : "O" }
+        { updateIsPending ? null : todo.status === "completed" ? "✔" : "O" }
       </Button>
       <div className="relative z-1">
         <p className={`break-all inline bg-[length:0px_0.1em] bg-[0px_center] ${todo.status === "completed" && "bg-gradient-to-l from-black to-black bg-no-repeat transition-all duration-1000 ease-in bg-[length:100%_0.1em]"}`}>
@@ -32,17 +27,12 @@ export default function Todo({ todo }: {todo: TodoType }) {
       </div>
       <Button
         type="button"
-        disabled={deleting}
-        onClick={() => {
-          setDeleting(true);
-          deleteTodo(todo)
-            .then(() => setDeleting(false))
-            .catch(() => setDeleting(false));
-        }}
+        disabled={deleteIsPending}
+        onClick={() => startDeleteTransition(async () => await deleteTodo(todo))}
         className={`text-[1.3rem] font-bold text-zinc-400 hover:text-zinc-500 focus:text-zinc-500 ml-auto px-2 rounded-md`}
-        showSpinner={deleting}
+        showSpinner={deleteIsPending}
       >
-        { deleting ? null : "X" }
+        { deleteIsPending ? null : "X" }
       </Button>
     </div>
   );
