be39945654cacde2588e8d597c12004536e8d2f7
ihccenen <ihccenen1@gmail.com>
2025-07-15 02:18:52 -0300

    Implement spinner for button and suspense for fetching todos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import { verifySession } from "./lib/session";
 import Navbar from "./ui/navbar";
 import TodoForm from "./ui/todo-form";
 import Todos from "./ui/todos";
+import Spinner from "./ui/spinner";
+import { Suspense } from "react";
 import Link from "next/link";
 
 export default async function Home() {
@@ -10,12 +12,14 @@ export default async function Home() {
   return (
     <main className="h-screen text-lg">
       { session
-        ? <div className="grid gap-5 pb-5">
+        ? <div className="h-full flex flex-col gap-5 pb-5">
             <Navbar username={session.username} />
-            <div className="container grid gap-2 p-2 mx-auto">
-              <TodoForm />
-              <Todos />
-            </div>
+            <Suspense fallback={<Spinner className="size-15 self-center grow" />}>
+              <div className="container grid gap-2 p-2 mx-auto">
+                <TodoForm />
+                <Todos />
+              </div>
+            </Suspense>
           </div>
         : <div className="h-full text-3xl text-blue-500 flex justify-center items-center gap-5 p-5">
             <Link href="/login" className="p-5 border-1 border-neutral-800 hover:bg-neutral-700 rounded-xl hover:underline">Log in</Link>
--- src/app/ui/button.tsx
+++ src/app/ui/button.tsx
@@ -1,3 +1,5 @@
+import Spinner from "./spinner";
+
 type ButtonProps = {
   type: "button" | "submit" | "reset" | undefined;
   disabled?: boolean;
@@ -15,21 +17,7 @@ export default function Button({ type, disabled = false, className = "", onClick
       onClick={onClick}
       className={`flex items-center gap-2 ${isPending ? "hover:cursor-wait" : disabled ? "hover:cursor-not-allowed" : "hover:cursor-pointer"} ${className}`}
     >
-      { isPending &&
-        <svg className="size-5 animate-spin text-white"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-        >
-          <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-          <path
-            className="opacity-75"
-            fill="currentColor"
-            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-          >
-          </path>
-        </svg>
-      }
+      { isPending && <Spinner />}
       {children}
     </button>
   );
